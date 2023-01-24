20c257e5d1c088a8a21aeb19e42598b8e4f0ba3a
IvanSukharukov <iva2742@mail.ru>
2023-01-24 20:46:34 +0300

    AdminLTE, layout, main view.

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+    <link rel="icon" href="{{ asset('adminlte/dist/img/preloader.png') }}">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }} ">
+    <!-- Ionicons -->
+    {{--    <link rel="stylesheet" href="{{ asset('adminlte/https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }} ">--}}
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css') }} ">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css') }} ">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/jqvmap/jqvmap.min.css') }} ">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }} ">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/overlayScrollbars/css/OverlayScrollbars.m') }} in.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/daterangepicker/daterangepicker.css') }} ">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/summernote/summernote-bs4.min.css') }} ">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+    <!-- Preloader -->
+    <div class="preloader flex-column justify-content-center align-items-center">
+        <img class="animation__shake" src="{{ asset('adminlte/dist/img/preloader.png') }}" alt="cMoney" height="60" width="60">
+        <p class="animation__shake">cMoney</p>
+    </div>
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+        </ul>
+
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="index3.html" class="brand-link">
+            {{--            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">--}}
+            <span class="brand-text font-weight-light">cMoney</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    {{--                    https://fontawesome.com/v5/search?o=r&m=free--}}
+                    <li class="nav-item">
+                        <a href="pages/gallery.html" class="nav-link active">
+                            {{--                            <i class="nav-icon far fa-image"></i>--}}
+                            <i class="nav-icon fas fa-money-bill-wave"></i>
+                            <p>
+                                Внести расход/доход
+                            </p>
+                        </a>
+                    </li>
+                    <!-- Add icons to the links using the .nav-icon class
+                         with font-awesome or any other icon font library -->
+                    <li class="nav-item menu-open">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Категории
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="./index.html" class="nav-link active">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Доход</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./index2.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Расход</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="./index3.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Счета</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+
+
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        @yield('content')
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; {{ now()->year }}  <a href="{{ route('main.index') }}">cMoney</a></strong>
+        <div class="float-right d-none d-sm-inline-block">
+            <b>controlMoney</b>
+        </div>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }} "></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="{{ asset('adminlte/plugins/jquery-ui/jquery-ui.min.js') }} "></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
+<!-- ChartJS -->
+{{--<script src="{{ asset('adminlte/plugins/chart.js/Chart.min.js') }} "></script>--}}
+<!-- Sparkline -->
+{{--<script src="{{ asset('adminlte/plugins/sparklines/sparkline.js') }} "></script>--}}
+<!-- JQVMap -->
+{{--<script src="{{ asset('adminlte/plugins/jqvmap/jquery.vmap.min.js') }} "></script>--}}
+{{--<script src="{{ asset('adminlte/plugins/jqvmap/maps/jquery.vmap.usa.js') }} "></script>--}}
+<!-- jQuery Knob Chart -->
+{{--<script src="{{ asset('adminlte/plugins/jquery-knob/jquery.knob.min.js') }} "></script>--}}
+<!-- daterangepicker -->
+<script src="{{ asset('adminlte/plugins/moment/moment.min.js') }} "></script>
+<script src="{{ asset('adminlte/plugins/daterangepicker/daterangepicker.js') }} "></script>
+<!-- Tempusdominus Bootstrap 4 -->
+{{--<script src="{{ asset('adminlte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-boot') }} strap-4.min.js"></script>--}}
+<!-- Summernote -->
+{{--<script src="{{ asset('adminlte/plugins/summernote/summernote-bs4.min.js') }} "></script>--}}
+<!-- overlayScrollbars -->
+{{--<script src="{{ asset('adminlte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }} "></script>--}}
+<!-- AdminLTE App -->
+<script src="{{ asset('adminlte/dist/js/adminlte.js') }} "></script>
+</body>
+</html>
